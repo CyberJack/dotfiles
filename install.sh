4b38e6377b52cebb358e32d3c8534d5d52920616
Peter Bottenberg <cyberjack77@gmail.com>
2020-02-03 16:11:17 +0100

    Update installer and made it executable

--- install.sh
+++ install.sh
@@ -1,21 +1,78 @@
 #!/usr/bin/env bash
 
-echo "Installing dotfiles"
+set -eo pipefail
 
-FORCED_INSTALL=false
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  FORCED_INSTALL=true
-fi
+TERM_SUPPORTS_COLORS=false
 
-if [[ "$FORCED_INSTALL" = false ]]; then
-  read -p -r "This may overwrite existing files in your home directory. Are you sure? [y/N] " -n 1;
-  echo "";
-  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    exit 1
-  fi
-fi
+function info {
+    [[ $TERM_SUPPORTS_COLORS = true ]] && echo -e "\033[0;32m$1\033[0m" || echo -e "$1"
+}
 
-rsync --exclude ".git/" \
-  --exclude "install.sh" \
-  --exclude "README.md" \
-  -avh --no-perms . ~;
+function error {
+    [[ $TERM_SUPPORTS_COLORS = true ]] && echo >&2 -e "\033[0;91m$1\033[0m" || echo >&2 -e "$1"
+}
+
+function ask {
+    read -r -p "$(echo -e "${1}") [y/N]): "
+    case $(echo "${REPLY}" | tr '[:upper:]' '[:lower:]') in
+        y|yes) echo "yes" ;;
+        *)     echo "no" ;;
+    esac
+}
+
+function header {
+    info "#############################"
+    info "#    Installing dotfiles    #"
+    info "#############################"
+    echo
+}
+
+function checkIfTermSupportsColors {
+    if command -v tput > /dev/null 2>&1 && [[ $(tput -T"$TERM" colors) -ge 8 ]]; then
+        TERM_SUPPORTS_COLORS=true
+    fi
+}
+
+function checkRequirements {
+    command -v rsync >/dev/null 2>&1 || {
+        error "The application \"rsync\" is required, but it's not installed. Aborting.";
+        exit 1;
+    }
+}
+
+function checkForcedInstall {
+    if [[ "$1" = "--force" || "$1" = "-f" ]]; then
+        info "Force installing dotfiles!. Any existing files will be overwritten!"
+        return
+    fi
+
+    if [[ -f "${HOME}/.zshrc" ]]; then
+        if [[ "yes" != $(ask "A zsh configuration already exists.\nContinuing will overwrite the existing configuration. Are you sure?") ]]; then
+            info "Aborting"
+            echo
+            exit 0
+        fi
+        echo
+    fi
+}
+
+function sync {
+    info "Installing dotfiles"
+    rsync -avhq \
+        --no-perms \
+        --exclude ".git/" \
+        --exclude ".gitignore" \
+        --exclude "install.sh" \
+        --exclude "README.md" \
+        ./ \
+        "${HOME}/"
+
+    echo
+    info "Dotfiles have been installed.\nRestart your terminal or (re)start ZSH and enjoy!"
+}
+
+checkIfTermSupportsColors
+header
+checkRequirements
+checkForcedInstall "${1:-false}"
+sync
