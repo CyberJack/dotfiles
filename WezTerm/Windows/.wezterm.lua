79627729080b6052df224429fb57a16eb22f650c
CyberJack <cyberjack77@gmail.com>
2025-09-03 17:01:07 +0200

    Add WezTerm configuration for Windows

---
+++ WezTerm/Windows/.wezterm.lua
@@ -0,0 +1,329 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+local act = wezterm.action
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- WSL 2
+config.wsl_domains = {
+  {
+    -- The name of this specific domain.  Must be unique amonst all types
+    -- of domain in the configuration file.
+    name = 'WSL:Ubuntu',
+
+    -- The name of the distribution.  This identifies the WSL distribution.
+    -- It must match a valid distribution from your `wsl -l -v` output in
+    -- order for the domain to be useful.
+    distribution = 'Ubuntu',
+  },
+}
+config.default_domain = 'WSL:Ubuntu'
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'Darcula'
+config.color_scheme = 'Molokai'
+config.font = wezterm.font('MesloLGS Nerd Font Mono')
+config.font_size = 10.5
+config.unzoom_on_switch_pane = true
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = true
+config.tab_bar_at_bottom = false
+config.hide_tab_bar_if_only_one_tab = false
+config.show_new_tab_button_in_tab_bar = true
+config.window_close_confirmation = "NeverPrompt"
+config.window_background_opacity = 1.0
+config.xcursor_theme = "Bibata-Modern-Ice"
+
+config.keys = {
+  {
+    key = ',',
+    mods = 'CMD',
+    action = act.SpawnCommandInNewTab {
+      cwd = os.getenv('WEZTERM_CONFIG_DIR'),
+      set_environment_variables = {
+        TERM = 'screen-256color',
+      },
+      args = {
+        'hx',
+        os.getenv('WEZTERM_CONFIG_FILE'),
+      },
+    },
+  },
+  {
+    key = ';',
+    mods = 'CMD',
+    action = act.SpawnCommandInNewTab {
+      args = {
+        'hx',
+        '~/.local/bin/run.sh',
+      },
+    },
+  },
+  {
+    key = 'RightArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+	key = 'DownArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+	key = 'w',
+    mods = 'CTRL',
+    action = wezterm.action.CloseCurrentPane { confirm = true },
+  },
+  {
+    key = '[',
+    mods = 'CMD',
+    action = act.Multiple {
+      act.ActivatePaneDirection 'Up',
+      act.EmitEvent 'reload-helix',
+    }
+  },
+  {
+    key = ']',
+    mods = 'CMD',
+    action = act.ActivatePaneDirection 'Down',
+  },
+  {
+    key = 'h',
+    mods = 'CMD|CTRL',
+    action = act.ActivatePaneDirection 'Left',
+  },
+  {
+    key = 'l',
+    mods = 'CMD|CTRL',
+    action = act.ActivatePaneDirection 'Right',
+  },
+  {
+    key = 'UpArrow',
+    mods = 'SHIFT',
+    action = act.ScrollByLine(-1),
+  },
+  {
+    key = 'DownArrow',
+    mods = 'SHIFT',
+    action = act.ScrollByLine(1),
+  },
+  {
+    key = 'Enter',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.TogglePaneZoomState,
+  },
+  {
+    key = 's',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.QuickSelectArgs {
+      label = 'open url',
+      patterns = {
+        'https?://\\S+',
+        '^/[^/\r\n]+(?:/[^/\r\n]+)*:\\d+:\\d+',
+        '[^\\s]+\\.rs:\\d+:\\d+',
+        'rustc --explain E\\d+',
+      },
+      action = wezterm.action_callback(function(window, pane)
+        local selection = window:get_selection_text_for_pane(pane)
+        wezterm.log_info('opening: ' .. selection)
+        if startswith(selection, "http") then
+          wezterm.open_with(selection)
+        elseif startswith(selection, "rustc --explain") then
+          local action = wezterm.action{
+            SplitPane={
+              direction = 'Right',
+              command = {
+                args = {
+                  '/bin/sh',
+                  '-c',
+                  'rustc --explain ' .. selection:match("(%S+)$") .. ' | mdcat -p',
+                },
+              },
+            };
+          };
+          window:perform_action(action, pane);
+        else
+          selection = "$EDITOR:" .. selection
+          return open_with_hx(window, pane, selection)
+        end
+      end),
+    },
+  },
+}
+
+function startswith(str, prefix)
+  return string.sub(str, 1, string.len(prefix)) == prefix
+end
+
+wezterm.on('reload-helix', function(window, pane)
+  local top_process = basename(pane:get_foreground_process_name())
+  if top_process == 'hx' then
+    local bottom_pane = pane:tab():get_pane_direction('Down')
+    if bottom_pane ~= nil then
+      local bottom_process = basename(bottom_pane:get_foreground_process_name())
+      if bottom_process == 'lazygit' then
+        local action = wezterm.action.SendString(':reload-all\r\n')
+        window:perform_action(action, pane);
+      end
+    end
+  end
+end)
+
+-- Keep the pane open after the program exits
+-- config.exit_behavior = "Hold"
+
+-- Honor kitty keyboard protocol: https://sw.kovidgoyal.net/kitty/keyboard-protocol/
+config.enable_kitty_keyboard = true
+
+for i = 1, 8 do
+  -- CTRL+ALT+number to move to that position
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'CTRL|ALT',
+    action = wezterm.action.MoveTab(i - 1),
+  })
+end
+
+config.set_environment_variables = {
+  PATH = '/Users/quantong/.cargo/bin:'
+      .. '/opt/homebrew/bin:'
+      .. os.getenv('PATH')
+}
+
+function extract_filename(uri)
+  local start, match_end = uri:find("$EDITOR:");
+  if start == 1 then
+    return uri:sub(match_end+1)
+  end
+
+  return nil
+end
+
+function editable(filename)
+  local extension = filename:match("%.([^.:/\\]+):%d+:%d+$")
+  if extension then
+    wezterm.log_info(string.format("extension is [%s]", extension))
+    local text_extensions = {
+      md = true,
+      c = true,
+      go = true,
+      scm = true,
+      rkt = true,
+      rs = true,
+    }
+    if text_extensions[extension] then
+      return true
+    end
+  end
+
+  return false
+end
+
+function extension(filename)
+  return filename:match("%.([^.:/\\]+):%d+:%d+$")
+end
+
+function basename(s)
+  return string.gsub(s, '(.*[/\\])(.*)', '%2')
+end
+
+function open_with_hx(window, pane, url)
+  local name = extract_filename(url)
+  wezterm.log_info('name: ' .. url)
+  if name and editable(name) then
+    if extension(name) == "rs" then
+      local pwd = string.gsub(pane:get_current_working_dir(), "file://.-(/.+)", "%1")
+      name = pwd .. "/" .. name
+    end
+
+    local direction = 'Up'
+    local hx_pane = pane:tab():get_pane_direction(direction)
+    if hx_pane == nil then
+      local action = wezterm.action{
+        SplitPane={
+          direction = direction,
+          command = { args = { 'hx', name } }
+        };
+      };
+      window:perform_action(action, pane);
+      pane:tab():get_pane_direction(direction).activate()
+    elseif basename(hx_pane:get_foreground_process_name()) == "hx" then
+      local action = wezterm.action.SendString(':open ' .. name .. '\r\n')
+      window:perform_action(action, hx_pane);
+      hx_pane:activate()
+    else
+      local action = wezterm.action.SendString('hx ' .. name .. '\r\n')
+      window:perform_action(action, hx_pane);
+      hx_pane:activate()
+    end
+    -- prevent the default action from opening in a browser
+    return false
+  end
+  -- otherwise, by not specifying a return value, we allow later
+  -- handlers and ultimately the default action to caused the
+  -- URI to be opened in the browser
+end
+
+wezterm.on('open-uri', function(window, pane, uri)
+  return open_with_hx(window, pane, uri)
+end)
+
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+table.insert(config.hyperlink_rules, {
+  regex = '^/[^/\r\n]+(?:/[^/\r\n]+)*:\\d+:\\d+',
+  format = '$EDITOR:$0',
+})
+
+table.insert(config.hyperlink_rules, {
+  regex = '[^\\s]+\\.rs:\\d+:\\d+',
+  format = '$EDITOR:$0',
+})
+
+mouse_bindings = {
+  {
+    event = { Down = { streak = 3, button = 'Left' } },
+    action = wezterm.action.SelectTextAtMouseCursor 'SemanticZone',
+    mods = 'NONE',
+  },
+  {
+    event = { Down = { streak = 1, button = "Right" } },
+    mods = "NONE",
+    action = wezterm.action_callback(function(window, pane)
+      local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+      if has_selection then
+        window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+        window:perform_action(act.ClearSelection, pane)
+      else
+        window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+      end
+    end),
+  },
+}
+
+config.foreground_text_hsb = {
+  hue = 1.0,
+  saturation = 1.2,
+  brightness = 1.5,
+}
+
+-- https://wezfurlong.org/wezterm/faq.html#multiple-characters-being-renderedcombined-as-one-character
+config.harfbuzz_features = { 'calt=0' }
+
+-- and finally, return the configuration to wezterm
+return config
